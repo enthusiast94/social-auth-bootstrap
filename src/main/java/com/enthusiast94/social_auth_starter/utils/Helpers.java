ca30d70c09bd7bdce79f7526c87fa15ba8ce3af7
Manas Bajaj <manas.bajaj94@gmail.com>
2015-07-28 12:03:39 +0530

    Added utility method for parsing http request body.

---
+++ src/main/java/com/enthusiast94/social_auth_starter/utils/Helpers.java
@@ -0,0 +1,20 @@
+package com.enthusiast94.social_auth_starter.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by ManasB on 7/28/2015.
+ */
+public class Helpers {
+
+    public static String bodyParams(String body, String key) {
+        Pattern pattern = Pattern.compile(key + "=" + "([^&]*)");
+        Matcher m = pattern.matcher(body);
+        if (m.find()) {
+            return m.group(1);
+        }
+
+        return null;
+    }
+}
