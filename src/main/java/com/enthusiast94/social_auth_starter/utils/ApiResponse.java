9298ea31447b5ed50c2dbcf88066fd75b82d9841
Manas Bajaj <manas.bajaj94@gmail.com>
2015-07-28 12:02:36 +0530

    Added utility class: ApiResponse, which will be used for returning responses with a specific format.

---
+++ src/main/java/com/enthusiast94/social_auth_starter/utils/ApiResponse.java
@@ -0,0 +1,20 @@
+package com.enthusiast94.social_auth_starter.utils;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by ManasB on 7/28/2015.
+ */
+
+public class ApiResponse {
+
+    private int status;
+    private String error;
+    private Object data;
+
+    public ApiResponse(int status, String error, Object data) {
+        this.status = status;
+        this.error = error;
+        this.data = data;
+    }
+}
