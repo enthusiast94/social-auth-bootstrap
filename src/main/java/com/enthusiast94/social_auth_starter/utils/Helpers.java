492316cd38d9044f8416d24ae1ed4cd1f7959783
Manas Bajaj <manas.bajaj94@gmail.com>
2015-08-01 23:50:22 +0530

    Added get and post helper methods.

--- src/main/java/com/enthusiast94/social_auth_starter/utils/Helpers.java
+++ src/main/java/com/enthusiast94/social_auth_starter/utils/Helpers.java
@@ -1,5 +1,11 @@
 package com.enthusiast94.social_auth_starter.utils;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,5 +22,75 @@ public class Helpers {
         }
 
         return null;
+    }
+
+    public static String stringifyParams(Map<String, String> queryParams) {
+        int pos = 0;
+
+        String paramsString = "";
+
+        for (String key : queryParams.keySet()) {
+            if (pos == 0) {
+                paramsString  += key + "=" + queryParams.get(key);
+            } else {
+                paramsString  += "&" + key + "=" + queryParams.get(key);
+            }
+
+            pos++;
+        }
+
+        return paramsString;
+    }
+
+    public static String httpGet(String urlString, Map<String, String> queryParams) throws Exception {
+        // add query params to url
+        urlString += "?" + stringifyParams(queryParams);
+
+        // setup url connection
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+
+        // read response
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder response = new StringBuilder();
+
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+
+        reader.close();
+
+        return response.toString();
+    }
+
+    public static String httpPost(String urlString, Map<String, String> postParams) throws Exception {
+        // setup url connection
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+
+        // convert post params to string
+        String postData = stringifyParams(postParams);
+
+        // write post data to connection
+        OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+        writer.write(postData);
+        writer.close();
+
+        // read response
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        StringBuilder response = new StringBuilder();
+
+        String line = "";
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+
+        reader.close();
+
+        return response.toString();
     }
 }
