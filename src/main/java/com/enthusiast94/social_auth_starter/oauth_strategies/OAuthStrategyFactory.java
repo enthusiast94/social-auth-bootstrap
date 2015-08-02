843189e138abac17ba2b7ec903b39380f89efa36
Manas Bajaj <manas.bajaj94@gmail.com>
2015-08-03 00:06:10 +0530

    [backend] Added a factory class which produces required OAuth strategies.

---
+++ src/main/java/com/enthusiast94/social_auth_starter/oauth_strategies/OAuthStrategyFactory.java
@@ -0,0 +1,42 @@
+package com.enthusiast94.social_auth_starter.oauth_strategies;
+
+import com.enthusiast94.social_auth_starter.services.AccessTokenService;
+import com.enthusiast94.social_auth_starter.services.UserService;
+
+import java.util.HashMap;
+
+/**
+ * Created by manas on 02-08-2015.
+ */
+
+public class OAuthStrategyFactory {
+
+    private UserService userService;
+    private AccessTokenService accessTokenService;
+    private HashMap<String, OAuthStrategy> strategies;
+
+    public OAuthStrategyFactory(UserService userService, AccessTokenService accessTokenService) {
+        this.userService = userService;
+        this.accessTokenService = accessTokenService;
+
+        strategies = new HashMap<>();
+        strategies.put("google", new GoogleOAuthStrategy(userService, accessTokenService));
+    }
+
+    public OAuthStrategy getStrategy(String name) {
+        if (!strategies.containsKey(name)) throw new IllegalArgumentException("No provider with name '" + name + "' found");
+
+        return strategies.get(name);
+    }
+
+    public HashMap<String, String> getAllAuthUrls() {
+        HashMap<String, String> authUrls = new HashMap<>();
+
+        for (String key : strategies.keySet()) {
+            authUrls.put(key, getStrategy(key).getAuthUrl());
+        }
+
+        return authUrls;
+    }
+
+}
